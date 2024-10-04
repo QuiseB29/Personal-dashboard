8981af5e65912a2631f2f0c6a5a7d9442a4ca70d
QuiseB29 <160094863+QuiseB29@users.noreply.github.com>
2024-10-03 23:22:25 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,25 @@ import Profile from './components/Profile';
 import './index.css';
 import PostsSection from './components/PostSection';
 import Form from './components/Form';
+import Albums from './components/Album'; // Import Albums component
+import TodoList from './components/Todo';
 
 // Create an Apollo Client instance
 const client = new ApolloClient({
-  uri: 'https://graphqlzero.almansi.me/api', // Replace with your GraphQL API URI
+  uri: 'https://graphqlzero.almansi.me/api', // GraphQL API URI
   cache: new InMemoryCache(),
 });
 
 const App: React.FC = () => {
+  const userId = "1"; // Replace with the actual user ID
 
   return (
     <ApolloProvider client={client}>
       <div className="App">
         <h1>Business Page</h1>
         <Form />
+        <TodoList />
+        <Albums userId={userId} /> {/* Pass userId as prop */}
         <Profile />
         <PostsSection />
       </div>
